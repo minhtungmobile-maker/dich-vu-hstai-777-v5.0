6de9bb781f5db9f70893a45431a814bc03c220f4
minhtungmobile-maker <minhtungmobile@gmail.com>
2026-03-01 22:56:09 +0700

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+﻿document.addEventListener("DOMContentLoaded", function() {
+  console.log("VMGS - HSTAI 777 System Initialized");
+
+  const links = document.querySelectorAll('a[href^="#"]');
+
+  links.forEach(link => {
+    link.addEventListener("click", function(e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute("href"))
+        .scrollIntoView({ behavior: "smooth" });
+    });
+  });
+});
